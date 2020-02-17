3ed3f6fcabc60e105b88297f0c59ddedbcbad072
Akira Kawamata <autumn@piedey.co.jp>
2020-02-17 19:07:54 +0900

    add Random

---
+++ Random/Random/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    private static DateTimeOffset InGameNow = new DateTimeOffset(2025, 1, 1, 12, 0, 0, TimeSpan.FromSeconds(0));
+    private static void Sub()
+    {
+        var r = new Random((int)InGameNow.Ticks);
+        Console.WriteLine(r.Next());
+        Console.WriteLine(r.Next());
+        Console.WriteLine(r.Next());
+    }
+
+
+    static void Main(string[] args)
+    {
+        Sub();
+    }
+}
