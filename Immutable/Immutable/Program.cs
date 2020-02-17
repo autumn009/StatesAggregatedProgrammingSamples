d2a658d1f27bdf87c2aab26a45128a37167852ca
Akira Kawamata <autumn@piedey.co.jp>
2020-02-17 15:14:35 +0900

    add Immutable

---
+++ Immutable/Immutable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Mutable
+{
+    private string name;
+    public string Name { get; set; } = "アイ";
+}
+
+class Immutable
+{
+    public string Name { get => "ルイ"; }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ai = new Mutable();
+        var rui = new Immutable();
+        ai.Name = "ルイ";
+        //rui.Name = "アイ"; 実行できない
+        Console.WriteLine(ai.Name);
+        Console.WriteLine(rui.Name);
+    }
+}
