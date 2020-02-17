1e26a3716de46399cd4e57c79c51fd99b1963a56
Akira Kawamata <autumn@piedey.co.jp>
2020-02-17 15:28:41 +0900

    add BadCase

---
+++ BadLocalVariable/BadLocalVariable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class BadCase
+{
+    public Action Sub()
+    {
+        int count=0;
+        return () =>
+        {
+            Console.WriteLine(count++);
+        };
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = new BadCase();
+        var act = a.Sub();
+        act();
+        act();
+        act();
+    }
+}
