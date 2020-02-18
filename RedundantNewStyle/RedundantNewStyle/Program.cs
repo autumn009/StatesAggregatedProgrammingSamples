b1716d223a2be4a911c61c1a1b187c88bab291f0
Akira Kawamata <autumn@piedey.co.jp>
2020-02-18 09:58:18 +0900

    add redundant

---
+++ RedundantNewStyle/RedundantNewStyle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class A
+{
+    public static int Price => State.Price;
+}
+
+class B
+{
+    public static string Price => State.Price + "yen";
+}
+
+class State
+{
+    public static int Price = 100;
+}
+
+//class BadState
+//{
+//  public static int Price = 100;
+//  public static string Price = 100;   // → Error
+//}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (A.Price >= 100) Console.WriteLine(B.Price);
+    }
+}
---
+++ RedundantOldStyle/RedundantOldStyle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class A
+{
+    public static string Price = "100yen";
+}
+
+class B
+{
+    public static int Price = 100;
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (B.Price >= 100) Console.WriteLine(A.Price);
+    }
+}
